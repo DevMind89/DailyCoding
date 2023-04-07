42aecad3fb35b5a8d7c6dd687a5cd623d63e2509
DevMind <30567851+DevMind89@users.noreply.github.com>
2023-04-07 16:17:06 +0200

    Update LowestPositive.java

--- LowestPositive.java
+++ LowestPositive.java
@@ -8,8 +8,6 @@ Given an array of integers, find the first missing positive integer in linear ti
 In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
 
 For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
-
-
 */
 
 import java.util.HashSet;
