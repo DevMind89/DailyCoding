8f927062842d37e204fa1f8b3846947229a1c4c2
DevMind <30567851+DevMind89@users.noreply.github.com>
2023-04-07 16:18:01 +0200

    Update MessageDecoder.java

--- MessageDecoder.java
+++ MessageDecoder.java
@@ -1,5 +1,4 @@
 /*
- 
 Good morning! Here's your coding interview problem for today.
 
 This problem was asked by Facebook.
@@ -9,7 +8,6 @@ Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the nu
 For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
 
 You can assume that the messages are decodable. For example, '001' is not allowed.
-
  */
 
 import java.util.HashMap;
